20b88a8d20875e7fe2cef76ea8eb949fa8b7bc81
wangdaxia <2049332830@qq.com>
2020-12-10 15:57:59 +0800

    日志

---
+++ src/main/java/com/example/cn/SpringTrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainApplication.class, args);
+	}
+
+}
