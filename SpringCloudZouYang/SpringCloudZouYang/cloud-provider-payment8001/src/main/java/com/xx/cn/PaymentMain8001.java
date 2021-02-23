1b25268d8e36d44215379e2536f11416691ac3f0
wangdaxia <2049332830@qq.com>
2021-02-23 16:37:34 +0800

    spring cloud

--- SpringCloudZouYang/SpringCloudZouYang/cloud-provider-payment8001/src/main/java/com/xx/cn/PaymentMain8001.java
+++ SpringCloudZouYang/SpringCloudZouYang/cloud-provider-payment8001/src/main/java/com/xx/cn/PaymentMain8001.java
@@ -2,11 +2,15 @@ package com.xx.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8001 {
 
 
--- SpringCloudZouYang/SpringCloudZouYang/cloud-provider-payment8001/src/main/java/com/xx/cn/controller/PaymentController.java
+++ SpringCloudZouYang/SpringCloudZouYang/cloud-provider-payment8001/src/main/java/com/xx/cn/controller/PaymentController.java
@@ -4,9 +4,14 @@ import com.xx.cn.entities.CommonResult;
 import com.xx.cn.entities.Payment;
 import com.xx.cn.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,15 +20,21 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
 
         if(result>0){
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功,serverPort: "+serverPort,result);
         }else {
-            return new CommonResult(444,"插入数据库失败",null);
+            return new CommonResult(444,"插入数据库失败,serverPort: "+serverPort,null);
         }
     }
 
@@ -33,9 +44,26 @@ public class PaymentController {
         log.info("*****查询结果："+payment);
 
         if(payment != null){
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功,serverPort: "+serverPort,payment);
         }else {
-            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
+            return new CommonResult(444,"没有对应记录，查询ID："+id+",serverPort: "+serverPort,null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String s:
+             services) {
+            log.info("******element: "+s);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance :
+                instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
